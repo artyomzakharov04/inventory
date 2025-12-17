1da3e05b126a98efe19397c29dad8fecef36aee3
artyomzakharov04 <160303103+artyomzakharov04@users.noreply.github.com>
2025-12-17 22:08:58 +0700

    Update test_items.py

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,24 +1,13 @@
-import sys
-import os
+from app import app
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from app import app, db
-
-
-def setup_module():
-    app.config["TESTING"] = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
-    with app.app_context():
-        db.create_all()
-
-
-def test_create_item():
+def test_add_item():
     client = app.test_client()
-    response = client.post("/items", json={
+    r = client.post("/items", json={
         "name": "Test",
-        "quantity": 10,
-        "price": 5,
+        "quantity": 1,
+        "price": 10,
         "category": "A"
     })
-    assert response.status_code == 201
+    assert r.status_code == 201
+
+
