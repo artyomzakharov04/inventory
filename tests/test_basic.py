1e5a64d98d6360f7b33c0f3c071245fceae36c92
artyomzakharov04 <160303103+artyomzakharov04@users.noreply.github.com>
2025-12-18 00:34:06 +0700

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,54 @@
-def test_dummy():
-    assert 1 == 1
+import pytest
+from app import app, db, Item
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+
+    with app.app_context():
+        db.create_all()
+        yield app.test_client()
+        db.drop_all()
+
+
+def test_create_and_get_items(client):
+    # создаём товар
+    response = client.post(
+        "/items",
+        json={
+            "name": "Test",
+            "quantity": 2,
+            "price": 100,
+            "category": "TestCat"
+        }
+    )
+    assert response.status_code == 201
+
+    # получаем список
+    response = client.get("/items")
+    assert response.status_code == 200
+
+    items = response.get_json()
+    assert len(items) == 1
+    assert items[0]["name"] == "Test"
+
+
+def test_update_quantity_put(client):
+    item = Item(
+        name="Phone",
+        quantity=5,
+        price=500,
+        category="Tech"
+    )
+    db.session.add(item)
+    db.session.commit()
+
+    response = client.put(
+        f"/items/{item.id}/quantity",
+        json={"delta": -1}
+    )
+
+    assert response.status_code == 200
+    assert response.get_json()["new_quantity"] == 4
